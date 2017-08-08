f161f70cfb9b5a3ce235af951ecb551ad8294c5d
Coby Lu <lucoby@gmail.com>
2017-08-07 21:31:39 -0400

    created test cases for RackO

---
+++ TestRackO.py
@@ -0,0 +1,13 @@
+import unittest
+
+from RackO import RackO
+
+class TestRackO(unittest.TestCase):
+
+    def test_rack_o(self):
+        rack_o = RackO(deck_size=10, hand_size=3, deck=[3, 1, 9, 6, 4, 7, 2, 10, 8, 5])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
